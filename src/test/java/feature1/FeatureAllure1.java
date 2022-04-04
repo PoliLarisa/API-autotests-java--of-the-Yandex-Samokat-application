a18c6c9de7a7b2adeafc28e2439e2d55254b85f1
Lara P <me2502283@gmail.com>
2022-04-04 19:42:13 +0300

    add the project

---
+++ src/test/java/feature1/FeatureAllure1.java
@@ -0,0 +1,10 @@
+package feature1;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+
+@Epic("Creating new user role")
+@Feature("Registration of courier")
+public class FeatureAllure1 {
+}
+
